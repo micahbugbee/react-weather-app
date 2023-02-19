9210228c6cfabef1f3da8116ce6260763eac93d6
micahbugbee <micahbugbee@htomail.com>
2023-02-19 10:57:28 -0600

    Initial commit with city search

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b6bb1dcfd8mshb97454653255261p13d578jsne005ed23d84e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+  };
+
+  export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
